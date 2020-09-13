84a5828117cdaa8f7df935a8114535ba8dc20217
Evan Kim <evan.e.kim@gmail.com>
2020-09-12 19:30:13 -0700

    added intro scene

--- firstProj.py
+++ firstProj.py
@@ -34,7 +34,53 @@ class Spring(VMobject):
     def get_vertices(self):
         return self.get_start_anchors()
 
-class start(Scene):
+class intro(Scene):
+    def construct(self):
+        dur=0.75
+        def homotopyTest(x,y,z,t):
+            Rotate(mainHexagon,PI*0.1)
+            if(t<dur):
+                return[x+4*t+2*t*t,y-2*t-t*t,z]
+            else:
+                return[x+4*dur+2*dur*dur+1.5*(t-dur),y-2*dur-dur*dur-3*(t-dur)*(t-dur)-5*(t-dur),z]
+        
+        mainHexagon = Polygon(ORIGIN,RIGHT,RIGHT*1.5+UP*math.sqrt(0.75),RIGHT+UP*math.sqrt(3),UP*math.sqrt(3),0.5*LEFT+UP*math.sqrt(0.75),color=WHITE,fill_color=WHITE)
+        plane = Polygon(LEFT*4+UP*2,LEFT*4+DOWN*2,RIGHT*4+DOWN*2,color=WHITE,fill_color=GRAY)
+        t = TextMobject("Slipping Hexagons")
+        t.scale(2)
+        title = TextMobject("Waves and Oscillations")
+        title.scale(2)
+        subtitle = TextMobject("Ep. 1: Simple Harmonic Motion")
+        subtitle.scale(1.2)
+        subtitle.move_to(DOWN*1.3)
+
+        self.play(FadeIn(plane),run_time=1.2)
+        self.play(FadeInFrom(mainHexagon,direction=3*UP),rate_func=rush_into,run_time=0.8);
+        self.add_sound("./hitSound.wav",gain=10);
+        #self.play(Homotopy(homotopyTest,mainHexagon))
+        self.play(Rotate(mainHexagon,-1*PI*26.57/180,about_point=ORIGIN),run_time=0.2)
+        self.add_sound("./hitSound.wav",gain=10);
+        # for i in range(1,3):
+        #     self.play(Rotate(mainHexagon,-1*PI/3,about_point=i*2/math.sqrt(5)*RIGHT + i/math.sqrt(5)*DOWN),run_time=0.6)
+        #     self.add_sound("./hitSound.wav",gain=10);
+        #self.add_sound("./Recording (17).m4a",gain=10,time_offset=0.5)
+        self.add_sound("./Recording (21).m4a",gain=10,time_offset=0.33)
+        #self.play(Homotopy(homotopyTest,mainHexagon))
+        self.play(Homotopy(homotopyTest,mainHexagon))
+        self.play(FadeOut(mainHexagon),FadeOut(plane),run_time=0.6)
+        self.wait(1)
+        # #self.wait(1.2)
+        # self.play(FadeOut(plane))
+        # #self.play(ReplacementTransform(plane,t))
+        # # #self.play(FadeOut(plane))
+        self.play(Write(t))
+        self.wait(0.3)
+        self.play(ReplacementTransform(t,title))
+        self.wait(0.4)
+        self.play(FadeIn(subtitle))
+        self.wait(0.4)
+
+class startScene(Scene):
     def construct(self):
         
         #Setting the backdrop
