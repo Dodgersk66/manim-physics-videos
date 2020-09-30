6fa487e5dc510be894bc15f59f5b8be165abad6b
Evan Kim <evan.e.kim@gmail.com>
2020-09-30 09:47:51 -0700

    added ioaa, astronomy pictures

--- astro-club-vid.py
+++ astro-club-vid.py
@@ -36,7 +36,8 @@ class twod_orbit(Scene):
             y_pos = satt_loc[1]
             satt_mag = np.sqrt(x_pos**2 + y_pos**2)
             angle = sattelite.angle
-
+            
+            #vis-viva
             vel_mag = np.sqrt(sattelite.adjustable_constant * (2/diff_vec_mag - 1/5))
 
             #vel_vec = (vel_mag/satt_mag) * (RIGHT*5 * np.sin(angle)+UP * 3*np.cos(angle))
@@ -62,11 +63,14 @@ class twod_orbit(Scene):
         ans2 = TextMobject("is the place for you!")
         ans2.move_to(0.3*DOWN)
 
-        for i in range(-6,7):
-            for j in range(-4,5):
-                locDot = Dot()
-                locDot.move_to(RIGHT*i + UP*j)
-                self.add(locDot)
+        # ioaaPic = ImageMobject("./images/ioaa-logo.png")
+        # ioaaPic.scale(8/ioaaPic.getWidth())
+
+        # for i in range(-6,7):
+        #     for j in range(-4,5):
+        #         locDot = Dot()
+        #         locDot.move_to(RIGHT*i + UP*j)
+        #         self.add(locDot)
         self.play(Write(quest))
         self.wait(1.5)
         self.play(ReplacementTransform(quest,quest2))
@@ -76,10 +80,36 @@ class twod_orbit(Scene):
         self.wait(1)
         self.play(FadeOut(ans),FadeOut(ans2))
         #self.play(FadeOut(ans2))
-
         
+        ioaa_tell = TextMobject("We compete in a series of olympiads")
+        ioaa_tell.move_to(3.5*UP)
+        ioaa_tell2 = TextMobject("that culminate to the IOAA!")
+        ioaa_tell2.next_to(ioaa_tell,direction=DOWN)
 
-        self.add(orbitEllipse,locDot,sun,sattelite,gravitation_force_vector)
+        ioaaPic = ImageMobject("./images/ioaa-logo.png")
+        ioaaPic.scale(8/ioaaPic.get_width())
+        self.play(Write(ioaa_tell))
+        self.play(Write(ioaa_tell2))
+        self.add(ioaaPic)
+        self.wait(2)
+        self.play(FadeOut(ioaa_tell),FadeOut(ioaa_tell2),FadeOut(ioaaPic))
+        
+        astro = TextMobject("So, obviously, we do astronomy:")
+        astro.move_to(3.5*UP)
+
+        astroPic = ImageMobject("./images/astro.jpg")
+        astroPic.scale(10/astroPic.get_width())
+
+        self.play(GrowFromCenter(astro))
+        self.add(astroPic)
+        self.wait(2)
+        self.play(FadeOut(astroPic),FadeOut(astro))
+        
+        astroPhys = TextMobject("And we do Astrophysics as well!")
+        astroPhys.move_to(3.5*UP)
+
+        self.add(orbitEllipse,sun,sattelite,gravitation_force_vector)
+        self.add(astroPhys)
         lineArray = []
         for i in range(0,15):
             newLine = Line(sun.get_center(),sattelite.get_center())
@@ -89,8 +119,15 @@ class twod_orbit(Scene):
         self.play(FadeOut(orbitEllipse),FadeOut(sun),FadeOut(sattelite))
         self.clear()
         self.wait(0.2)
-
-
+        
+        final = TextMobject("So, join the newest and coolest club at STEM!")
+        final2  = TextMobject("-The Competitive Astronomy Club Team")
+        final2.set_color(YELLOW)
+        final2.scale(0.6)
+        final2.move_to(2.2*RIGHT+0.5*DOWN)
+        self.play(Write(final))
+        self.play(Write(final2),run_time = 1.5)
+        self.wait(1)
 
         # line1 = Line(sun.get_center(),sattelite.get_center())
         
