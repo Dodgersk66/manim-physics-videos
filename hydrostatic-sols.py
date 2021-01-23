688ba7b3192ee7669d52acda106d4ea777ccb40c
Evan Kim <evan.e.kim@gmail.com>
2021-01-22 22:51:58 -0800

    started hydrostatic sols

--- hydrostatic-sols.py
+++ hydrostatic-sols.py
@@ -16,7 +16,7 @@ class Logo(Scene):
         t = TextMobject("Slipping Hexagons")
         t.scale(2)
         t.move_to(DOWN*1.4)
-        title = TextMobject("Hydrostatic Puzzles!")
+        title = TextMobject("Hydrostatic Solutions!")
         title.scale(2)
 
         logo = ImageMobject("./Images/logo4.0.png")
@@ -74,23 +74,23 @@ class Container(VMobject):
 class HydrostaticParadox(Scene):
     def construct(self):
         
-        # for i in range(-8, 9):
-        #     for j in range(-4,5):
-        #         if(i == 0 and j == 0):
-        #             self.add(Dot(color = RED))
-        #             continue;
-        #         self.add(Dot(i * RIGHT + j * UP))
+        for i in range(-8, 9):
+            for j in range(-4,5):
+                if(i == 0 and j == 0):
+                    self.add(Dot(color = RED))
+                    continue;
+                self.add(Dot(i * RIGHT + j * UP))
 
-        title = TextMobject("Problem 1: ", "Hydrostatic Paradox")
+        title = TextMobject("Solution 1: ", "Hydrostatic Paradox")
         title.set_color_by_tex_to_color_map({
-            "Problem 1: " : YELLOW,
+            "Solution 1: " : YELLOW,
             "Hydrostatic Paradox" : WHITE
         })
         title.scale(1.6)
 
-        titleScaledAndMoved = TextMobject("Problem 1: ", "Hydrostatic Paradox")
+        titleScaledAndMoved = TextMobject("Solution 1: ", "Hydrostatic Paradox")
         titleScaledAndMoved.set_color_by_tex_to_color_map({
-            "Problem 1: " : YELLOW,
+            "Solution 1: " : YELLOW,
             "Hydrostatic Paradox" : WHITE
         })
         titleScaledAndMoved.move_to(3.3 * UP + 3 * LEFT)
@@ -101,107 +101,20 @@ class HydrostaticParadox(Scene):
 
         #wide
         container1 = Container(5.3 * LEFT + UP, 5 * LEFT + 2 * DOWN, 3 * LEFT + 2 * DOWN, 2.7 * LEFT + UP,fill_opacity= 0)
-        botLength1 = Line(start = 5 * LEFT + 2.3 * DOWN, end = 3 * LEFT + 2.3 *DOWN)
-        botLength1.add_tip(at_start=  True)
-        botLength1.add_tip()
-        botLength1.set_color(BLUE)
-        botLength1Label = DecimalNumber(4.00)
-        botLength1Label.scale(0.6)
-        botLength1Label.move_to(2.6 * DOWN + 4 * LEFT)
-        
-        def label_updater(d,dt):
-            botLength1Label.set_value(2 * (botLength1.get_right() - botLength1.get_left())[0])
-            botLength1Label.align_to(botLength1.get_bottom() - 0.2 * UP, direction = UP)
-        def arrow_updater(d,dt):
-            if(d.get_width() <= 2.7):
-                d.move_to(d.get_center() + dt * UP )
-                d.stretch_to_fit_width(d.get_width() + 0.6 * dt / 3)
 
 
         #normal
         container2 = Container(LEFT + UP, LEFT + 2 * DOWN, RIGHT + 2 * DOWN, RIGHT + UP,fill_opacity= 0)
-        water2 = Container(LEFT + UP, LEFT + 2 * DOWN, RIGHT + 2 * DOWN, RIGHT + UP)
-        water2.time = 0
-        water1 = Container(LEFT + UP, LEFT + 2 * DOWN, RIGHT + 2 * DOWN, RIGHT + UP)
-        water1.time = 0
-        water3 = Container(LEFT + UP, LEFT + 2 * DOWN, RIGHT + 2 * DOWN, RIGHT + UP)
-        water3.time = 0
-
-        def water2Update(d,dt):
-            tim = water2.time / 12
-            water2.set_points_as_corners([LEFT +2 * DOWN + UP * ( 2.6 - 2.6 * np.exp(-tim)),LEFT + 2 * DOWN, RIGHT + 2 * DOWN,RIGHT +2 * DOWN + UP * ( 2.6- 2.6 * np.exp(-tim))])
-            water2.time += 1
-        def water1Update(d,dt):
-            tim = water1.time / 12
-            water1.set_points_as_corners([5 * LEFT +2 * DOWN + UP * ( 2.6 - 2.6 * np.exp(-tim)) + (0.3 * 2.6/3- 0.3* 2.6/3 * np.exp(-tim)) * LEFT,5 * LEFT + 2 * DOWN, 3 * LEFT + 2 * DOWN,3 * LEFT +2 * DOWN + UP * ( 2.6 - 2.6 * np.exp(-tim)) + (0.3 * 2.6/3 - 0.3 * 2.6/3* np.exp(-tim)) * RIGHT])
-            water1.time += 1
-        def water3Update(d,dt):
-            tim = water3.time / 12
-            water3.set_points_as_corners([3 * RIGHT +2 * DOWN + UP * ( 2.6 - 2.6 * np.exp(-tim)) + (0.3 * 2.6/3 - 0.3* 2.6/3 * np.exp(-tim)) * RIGHT, 3 * RIGHT + 2 * DOWN, 5 * RIGHT + 2 * DOWN,5 * RIGHT +2 * DOWN + UP * ( 2.6 - 2.6 * np.exp(-tim)) + (0.3* 2.6/3 - 0.3 * 2.6/3* np.exp(-tim)) * LEFT])
-            water3.time += 1
-
-        botLength2 = Line(start = LEFT+ 2.3 * DOWN, end = RIGHT + 2.3 *DOWN)
-        botLength2.add_tip(at_start=  True)
-        botLength2.add_tip()
-        botLength2.set_color(BLUE)
-        botLength2Label = DecimalNumber(4.00)
-        botLength2Label.scale(0.6)
-        botLength2Label.move_to(2.6 * DOWN)
-
-        def label_updater2(d,dt):
-            
-            botLength2Label.align_to(botLength2.get_bottom() - 0.2 * UP, direction = UP)
-        def arrow_updater2(d,dt):
-            if(botLength1.get_width() <= 2.7):
-                d.move_to(d.get_center() + dt * UP)
-                #d.stretch_to_fit_width(d.get_width() + 0.6 * dt / 3 * 2)
-
+        water2 = Container(LEFT +2 * DOWN + UP * ( 2.6),LEFT + 2 * DOWN, RIGHT + 2 * DOWN,RIGHT +2 * DOWN + UP * ( 2.6))
+        water1 = Container(5 * LEFT +2 * DOWN + UP * ( 2.6 - 2.6 * 0) + (0.3 * 2.6/3- 0.3* 2.6/3 * 0) * LEFT,5 * LEFT + 2 * DOWN, 3 * LEFT + 2 * DOWN,3 * LEFT +2 * DOWN + UP * ( 2.6 - 2.6 * 0) + (0.3 * 2.6/3 - 0.3 * 2.6/3* 0) * RIGHT)
+        water3 = Container(3 * RIGHT +2 * DOWN + UP * ( 2.6 - 2.6 * 0) + (0.3 * 2.6/3 - 0.3* 2.6/3 * 0) * RIGHT, 3 * RIGHT + 2 * DOWN, 5 * RIGHT + 2 * DOWN,5 * RIGHT +2 * DOWN + UP * ( 2.6 - 2.6 * 0) + (0.3* 2.6/3 - 0.3 * 2.6/3* 0) * LEFT)
+        
         #thin
         container3 = Container(3.3 * RIGHT + UP, 3 * RIGHT + 2 * DOWN, 5* RIGHT +  2 * DOWN, 4.7 * RIGHT + UP,fill_opacity= 0)
         
-        
-        botLength3 = Line(start = 3 * RIGHT+ 2.3 * DOWN, end = 5 * RIGHT + 2.3 *DOWN)
-        botLength3.add_tip(at_start=  True)
-        botLength3.add_tip()
-        botLength3.set_color(BLUE)
-        botLength3Label = DecimalNumber(4.00)
-        botLength3Label.scale(0.6)
-        botLength3Label.move_to(4 * RIGHT + 2.6 * DOWN)
-
         table = Polygon(8 * LEFT + 2.02 * DOWN, 8 * RIGHT + 2.02 * DOWN, 8 * RIGHT + 5.2 * DOWN, 8 * LEFT + 5.2 * DOWN,fill_opacity = 1, fill_color = "#222222", stroke_width = 0, sheen_factor=  0.6, sheen_direction = UP)
 
-        def label_updater3(d,dt):
-            botLength3Label.set_value(2 * (botLength3.get_right() - botLength3.get_left())[0])
-            botLength3Label.align_to(botLength3.get_bottom() - 0.2 * UP, direction = UP)
-        def arrow_updater3(d,dt):
-            if(botLength1.get_width() <= 2.7):
-                d.move_to(d.get_center() + dt * UP)
-                d.stretch_to_fit_width(d.get_width() - 0.6 * dt / 3)
-
-
-        self.play(DrawBorderThenFill(table))
-
-        self.play(DrawBorderThenFill(container1),DrawBorderThenFill(container2),DrawBorderThenFill(container3))
-        self.wait(1)
-
-        self.play(FadeIn(botLength1),FadeIn(botLength2),FadeIn(botLength3),FadeIn(botLength1Label),FadeIn(botLength2Label),FadeIn(botLength3Label))
-        botLength1Label.add_updater(label_updater)
-        botLength1.add_updater(arrow_updater)
-        botLength2Label.add_updater(label_updater2)
-        botLength2.add_updater(arrow_updater2)
-        botLength3Label.add_updater(label_updater3)
-        botLength3.add_updater(arrow_updater3)
-
-        self.add(botLength1Label,botLength1,botLength2Label,botLength2,botLength3Label,botLength3)
-        self.wait(7)
-        self.play(FadeOut(botLength1Label),FadeOut(botLength1),FadeOut(botLength2Label),FadeOut(botLength2),FadeOut(botLength3Label),FadeOut(botLength3))
-        water2.add_updater(water2Update)
-        water3.add_updater(water3Update)
-        water1.add_updater(water1Update)
-        self.add(water2,water1,water3)
-        self.wait(3)
         waterLine = DashedLine(6 * LEFT + 0.6 * UP, 6 * RIGHT + 0.6 * UP)
-        self.play(DrawBorderThenFill(waterLine))
 
         hydroEq = TexMobject("\\frac{\\mathrm d P}{\mathrm d z} = - \\rho g")
         hydroEq2 = TexMobject("\\Delta P = - \\rho g \\Delta z")
@@ -210,7 +123,6 @@ class HydrostaticParadox(Scene):
         hydroEq2.move_to(3 * UP + 4 * RIGHT)
         pressureEq.move_to(2 * UP + 4 * RIGHT)
 
-        relation1 = TexMobject("PA \\, \\, \\qquad = \\, \\qquad \\, PA \\,\\qquad\\,   = \\, \\qquad \\, PA")
         relation2 = TexMobject("F_1 \\,\\, \\,\\qquad\\, = \\,\\qquad \\,\\,\\, F_2\\, \\,\\,\\,\\qquad = \\,\\,\\,\\, \\qquad F_3 ")
 
         f1 = Line(4 * LEFT + DOWN, 4 * LEFT + 2 * DOWN,color = GREEN)
@@ -221,21 +133,7 @@ class HydrostaticParadox(Scene):
         f3.add_tip()
 
 
-        relation1.move_to(3 * DOWN)
         relation2.move_to(3 * DOWN)
-        self.play(Write(hydroEq))
-        self.play(ReplacementTransform(hydroEq,hydroEq2))
-        self.play(Write(pressureEq))
-        self.play(GrowFromPoint(f1,f1.get_top()),GrowFromPoint(f2,f2.get_top()),GrowFromPoint(f3,f3.get_top()))
-        self.wait(1)
-        self.play(Write(relation1),run_time = 2)
-        self.play(ReplacementTransform(relation1,relation2))
-        self.wait(1)
-        self.play(Indicate(container1),run_time = 1.8)
-        self.play(Indicate(container2),run_time = 1.8)
-        self.play(Indicate(container2),run_time = 1.8)
-        self.play(Indicate(container3),run_time = 1.8)
-        self.wait(1)
 
         question = TextMobject("If the volumes are different,") 
         question2 = TextMobject("how can the forces at the bottoms be the same?")
@@ -247,9 +145,16 @@ class HydrostaticParadox(Scene):
         question2.move_to(1.9 * UP)
         question2.align_to(6 * LEFT, LEFT)
 
-        self.play(Write(question))
-        self.play(Write(question2))
+
+        self.play(FadeIn(table),FadeIn(container1),FadeIn(container2),FadeIn(container3),FadeIn(water1),FadeIn(water2),FadeIn(water3),FadeIn(waterLine),FadeIn(hydroEq2),
+                    FadeIn(pressureEq),FadeIn(relation2),FadeIn(f1),FadeIn(f2),FadeIn(f3),FadeIn(question),FadeIn(question2))
         self.wait(1)
+
+        ### Solution from here ###
+
+        
+
+
     
 
         
@@ -257,12 +162,12 @@ class HydrostaticParadox(Scene):
 
 class Problem2(Scene):
     def construct(self):
-        # for i in range(-16, 17):
-        #     for j in range(-8,9):
-        #         if(i == 0 and j == 0):
-        #             self.add(Dot(color = RED))
-        #             continue;
-        #         self.add(Dot(i / 2 * RIGHT + j / 2 * UP))
+        for i in range(-16, 17):
+            for j in range(-8,9):
+                if(i == 0 and j == 0):
+                    self.add(Dot(color = RED))
+                    continue;
+                self.add(Dot(i / 2 * RIGHT + j / 2 * UP))
 
         title = TextMobject("Problem 2: ", "A famous see-saw")
         title.set_color_by_tex_to_color_map({
@@ -336,52 +241,59 @@ class Problem2(Scene):
         self.add(title)
         self.play(ReplacementTransform(title,titleScaledAndMoved))
         self.wait(1)
-        self.play(DrawBorderThenFill(table))
+
+
+        self.play(FadeIn(table),FadeIn(fulcrum),FadeIn(balance),FadeIn(plate1),FadeIn(plate2),FadeIn(container1),FadeIn(container2),FadeIn(water1),FadeIn(water2),
+                   FadeIn(pingPong),FadeIn(rock),FadeIn(stand),FadeIn(stringPing),FadeIn(stringRock),FadeIn(questionPart2))
+        
+        ### Solution from here ###
+
+        larr1 = Line(3.5 * LEFT + 0.7 * UP, 3.5 * LEFT + 0.25 * DOWN,color = BLACK)
+        larr2 = Line(3 * LEFT+ 0.7 * UP, 3 * LEFT + 0.25 * DOWN,color = BLACK)
+        larr3 = Line(2.5 * LEFT + 0.7 * UP, 2.5 * LEFT + 0.25 * DOWN,color = BLACK)
+        rarr1 = Line(3.5 * RIGHT + 0.7 * UP, 3.5 * RIGHT + 0.25 * DOWN,color = BLACK)
+        rarr2 = Line(3 * RIGHT+ 0.7 * UP, 3 * RIGHT + 0.25 * DOWN,color = BLACK)
+        rarr3 = Line(2.5 * RIGHT + 0.7 * UP, 2.5 * RIGHT + 0.25 * DOWN,color = BLACK)
+
+        larr1.add_tip()
+        larr2.add_tip()
+        larr3.add_tip()
+        rarr1.add_tip()
+        rarr2.add_tip()
+        rarr3.add_tip()
+        
+        self.play(Indicate(container1),Indicate(container2))
         self.wait(1)
-        self.play(DrawBorderThenFill(fulcrum))
+        self.play(GrowFromPoint(larr1,larr1.get_top()),GrowFromPoint(larr2,larr2.get_top()),GrowFromPoint(larr3,larr3.get_top()),GrowFromPoint(rarr1,rarr1.get_top()),GrowFromPoint(rarr2,rarr2.get_top()),GrowFromPoint(rarr3,rarr3.get_top()))       
+
+        waterLine = DashedLine(4.8 * LEFT + 1.25 * UP, 4.8 * RIGHT + 1.25 * UP)
+
         self.wait(1)
-        self.play(FadeIn(balance))
-        self.play(FadeIn(plate1),FadeIn(plate2))
+        self.play(Indicate(pingPong))
         self.wait(1)
-        self.play(DrawBorderThenFill(container1),DrawBorderThenFill(container2))
+        self.play(Indicate(rock))
         self.wait(1)
-        self.play(FadeIn(water1),FadeIn(water2))
-        self.wait(1)
-        self.play(DrawBorderThenFill(pingPong))
-        self.wait(1)
-        self.play(DrawBorderThenFill(rock))
+        self.play(DrawBorderThenFill(waterLine))
         self.wait(1)
         
-        pingForce = Arrow(pingPong.get_center()+ 0.3 * DOWN,pingPong.get_center() + 1.3 * UP+ 0.3 * DOWN,color = GREEN)
-        rockForce = Arrow(rock.get_center() + 0.3 * UP,rock.get_center() + 1.3 * DOWN+ 0.3 * UP,color = GREEN)
+    
+        leftEq = TexMobject("F_L = F_P - F_T")
+        rightEq = TexMobject("F_R = F_P")
+        ineq = TexMobject("F_L < F_R")
 
-        self.play(GrowFromPoint(pingForce,pingPong.get_center()),GrowFromPoint(rockForce,rock.get_center()))
+        self.play(FadeOut(larr1),FadeOut(larr2),FadeOut(larr3),FadeOut(rarr1),FadeOut(rarr2),FadeOut(rarr3))
 
-        self.wait(1)
-        self.play(FadeOut(pingForce),FadeOut(rockForce))
-        self.play(DrawBorderThenFill(stand))
-        self.wait(1)
-        self.play(GrowFromPoint(stringPing,stringPing.get_bottom()),FadeIn(stringRock,stringRock.get_top()))
-        self.wait(1)
-        self.play(Write(question))
-        self.wait(1)
-        self.play(Rotate(balance, angle = -PI/22, about_point = DOWN),Rotate(plate1, angle = -PI/22, about_point = DOWN),Rotate(balance, angle = -PI/22, about_point = DOWN),
-                Rotate(plate2, angle = -PI/22, about_point = DOWN),Rotate(container1, angle = -PI/22, about_point = DOWN),Rotate(container2, angle = -PI/22, about_point = DOWN),
-                Rotate(water1, angle = -PI/22, about_point = DOWN),Rotate(water2, angle = -PI/22, about_point = DOWN),Rotate(stringPing, angle = -PI/22, about_point = DOWN),Rotate(pingPong, angle = -PI/22, about_point = DOWN))
-        self.play(Rotate(balance, angle = PI/22, about_point = DOWN),Rotate(plate1, angle = PI/22, about_point = DOWN),Rotate(balance, angle = PI/22, about_point = DOWN),
-                Rotate(plate2, angle = PI/22, about_point = DOWN),Rotate(container1, angle = PI/22, about_point = DOWN),Rotate(container2, angle = PI/22, about_point = DOWN),
-                Rotate(water1, angle = PI/22, about_point = DOWN),Rotate(water2, angle = PI/22, about_point = DOWN),Rotate(stringPing, angle = PI/22, about_point = DOWN),Rotate(pingPong, angle = PI/22, about_point = DOWN))
+        tension = Line(3 * LEFT + 0.25 * DOWN, 3 * LEFT + 0.25 * UP,color = BLACK)
+        tension.add_tip()
+        self.play(GrowFromPoint(tension,tension.get_bottom()))
         
-        self.wait(1)
-        self.play(Write(questionPart2))
-        self.wait(1)
-        self.play(Rotate(balance, angle = PI/22, about_point = DOWN),Rotate(plate1, angle = PI/22, about_point = DOWN),Rotate(balance, angle = PI/22, about_point = DOWN),
-                Rotate(plate2, angle = PI/22, about_point = DOWN),Rotate(container1, angle = PI/22, about_point = DOWN),Rotate(container2, angle = PI/22, about_point = DOWN),
-                Rotate(water1, angle = PI/22, about_point = DOWN),Rotate(water2, angle = PI/22, about_point = DOWN),Rotate(stringPing, angle = PI/22, about_point = DOWN),Rotate(pingPong, angle = PI/22, about_point = DOWN))
-        self.play(Rotate(balance, angle = -PI/22, about_point = DOWN),Rotate(plate1, angle = -PI/22, about_point = DOWN),Rotate(balance, angle = -PI/22, about_point = DOWN),
-                Rotate(plate2, angle = -PI/22, about_point = DOWN),Rotate(container1, angle = -PI/22, about_point = DOWN),Rotate(container2, angle = -PI/22, about_point = DOWN),
-                Rotate(water1, angle = -PI/22, about_point = DOWN),Rotate(water2, angle = -PI/22, about_point = DOWN),Rotate(stringPing, angle = -PI/22, about_point = DOWN),Rotate(pingPong, angle = -PI/22, about_point = DOWN))
-        self.wait(1)
+
+        correct = Ellipse(width = 1.3, height = 0.7,color = GREEN)
+        correct.move_to(2.25 * UP + 0.6 * LEFT)
+
+        self.play(DrawBorderThenFill(correct))
+
+
 
 class Perpet(Scene):
     def construct(self):
